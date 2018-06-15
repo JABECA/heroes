ff1363657f44dc332c82ba0be6b72b548bac6bb1
Alejandro <alejandromym@utp.edu.co>
2018-06-15 07:42:25 +0100

    Homework2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Mr. Cook' },
+	{ id: 12, name: 'Narcotico' },
+	{ id: 13, name: 'Bombaso' },
+	{ id: 14, name: 'Cerebritos' },
+	{ id: 15, name: 'Magneto' },
+	{ id: 16, name: 'Wolberine' },
+	{ id: 17, name: 'Dynamo' },
+	{ id: 18, name: 'Dr FQ' },
+	{ id: 19, name: 'Pacman' },
+	{ id: 20, name: 'Trueno' },
+];
